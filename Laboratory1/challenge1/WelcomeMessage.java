a92f645bdd2409b110763d0cd5b7100fa431ce1d
RubianoJ <juanita.rubiano-f@mail.escuelaing.edu.co>
2026-08-05 21:42:05 -0500

    Comentarios en orden

--- Laboratory1/challenge1/WelcomeMessage.java
+++ Laboratory1/challenge1/WelcomeMessage.java
@@ -2,12 +2,16 @@
 import java.util.List;
 import java.util.stream.Collectors;
 
-//Clase que maneja la estructura y organizacion del mensaje 
+/**
+ * Clase que maneja la estructura y organizacion del mensaje
+ */
 public class WelcomeMessage {
-    // ponemos el dominio del correo como Static Final porque siempre es el mismo,
-    // se peude completar
-    // Ademas de que el dia que cambie, solo debemos cambiar una unica cosa en
-    // codigo.
+    /**
+     * ponemos el dominio del correo como Static Final porque siempre es el mismo,
+     * se peude completar
+     * Ademas de que el dia que cambie, solo debemos cambiar una unica cosa en
+     * codigo
+     */
     private static final String DOMAIN = "@escuelaing.edu.co";
 
     private List<Student> students;
