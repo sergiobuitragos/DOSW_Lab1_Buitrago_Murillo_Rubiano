e650cbb4159bcbc0a5b7cd4451d6cbf5a44be25f
Miguel Angel Murillo Hurtado <miguel.murillo-h@mail.escuelaing.edu.co>
2026-08-06 10:48:02 -0500

    funcion lambda findMax en Lane 1

--- Laboratory1/challenge2/Parallel_Race.java
+++ Laboratory1/challenge2/Parallel_Race.java
@@ -1,8 +1,17 @@
-import java.util.List;
+package challenge2;
 
+import java.util.List;
+import java.util.function.Function;
 public class Parallel_Race{
+
+    //Lane one: finds the max value in a list
+    Function<List<Integer>, Integer> findMax = (list) -> list.stream().mapToInt(Integer::intValue).max().orElseThrow();
+
     public static void main(String[] args){
         List<Integer> numbers = List.of(5, 3, 8, 1, 9, 2, 7, 4, 6);
         System.out.println("Numbers: " + numbers);
+
+        Parallel_Race race = new Parallel_Race();
+        System.out.println("Max value: " + race.findMax.apply(numbers));
     }
 }
