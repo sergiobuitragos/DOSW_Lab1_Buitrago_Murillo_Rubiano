a665f7df076be59f9e94c59b647556b543143230
Miguel Angel Murillo Hurtado <miguel.murillo-h@mail.escuelaing.edu.co>
2026-08-06 15:36:12 -0500

    feat: add Hashtable method for challenge 4 Student B

--- Laboratory1/challenge4/Challenge4.java
+++ Laboratory1/challenge4/Challenge4.java
@@ -1,38 +1,47 @@
 package challenge4;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 /**
- * guarda clave-valor en HashMap he ignorando las claves repetidas
+ * guarda clave-valor en HashMap e ignorando las claves repetidas
  */
 public class Challenge4 {
 
     /**
-     * Agrega una clave y un valor al mapa.
-     * @param map 
-     * @param key 
-     * @param value 
+     * A - Agrega una clave y un valor al HashMap.
      */
     public static void addTreasure(Map<String, Integer> map, String key, Integer value) {
-
         if (!map.containsKey(key)) {
             map.put(key, value);
         }
     }
 
     /**
-     * clase principal
-     * @param args
+     * B - Agrega una clave y un valor al Hashtable.
      */
+    public static void addToHashtable(Hashtable<String, Integer> table, String key, Integer value) {
+        if (!table.containsKey(key)) {
+            table.put(key, value);
+        }
+    }
+
     public static void main(String[] args) {
-
+        //HashMap
         HashMap<String, Integer> treasures = new HashMap<>();
+        addTreasure(treasures, "gold", 5);
+        addTreasure(treasures, "silver", 3);
+        addTreasure(treasures, "gold", 7);    // ignored
+        addTreasure(treasures, "diamond", 10);
+        System.out.println("HashMap: " + treasures);
 
-        addTreasure(treasures, "oro", 100);
-        addTreasure(treasures, "plata", 50);
-        addTreasure(treasures, "oro", 200); // Se ignora
-
-        System.out.println(treasures);
+        //Hashtable
+        Hashtable<String, Integer> hashTable = new Hashtable<>();
+        addToHashtable(hashTable, "silver", 8);
+        addToHashtable(hashTable, "ruby", 4);
+        addToHashtable(hashTable, "gold", 12);
+        addToHashtable(hashTable, "emerald", 6);
+        System.out.println("Hashtable: " + hashTable);
     }
 }
